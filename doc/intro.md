9a8b7db1bd985df59353b7facb8753a61e18fc47
Paul Walker <paul@pwjw.com>
2018-10-10 19:55:01 -0400

    Raw lein

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to exs-handler
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/exs_handler/core_test.clj
@@ -0,0 +1,7 @@
+(ns exs-handler.core-test
+  (:require [clojure.test :refer :all]
+            [exs-handler.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
