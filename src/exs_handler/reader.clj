5eabf1ff1d20d616bd95f370212bef2058d842e9
Paul Walker <paul@pwjw.com>
2018-10-11 15:31:04 -0400

    can read even more now

--- src/exs_handler/reader.clj
+++ src/exs_handler/reader.clj
@@ -6,41 +6,73 @@
 ;; TODO: Multimethod reader
 (defmulti unpack-chunk :sig)
 
+(defn unpack-i [d o]
+  (let [bb (ByteBuffer/allocate 4)
+        _  (doto bb
+             (.order (ByteOrder/LITTLE_ENDIAN))
+             (.put (byte-array d) o 4)
+             (.flip)
+             )
+        ]
+    (.getInt bb)
+    )
+  )
+
+(defn unpack-b [d o]
+  (nth d o)
+  )
+(defn unpack-s [d o]
+  (String. (byte-array (take-while (comp not zero?) (drop o d))) "UTF-8")
+  )
+
+(defn assoc-i [dat m k o] (assoc m k (unpack-i dat o)))
+(defn assoc-b [dat m k o] (assoc m k (unpack-b dat o)))
+(defn assoc-s [dat m k o] (assoc m k (unpack-s dat o)))
 
 
 (defmethod unpack-chunk :zone [dat]
-  (let [bb (ByteBuffer/allocate (:size dat))
-        _  (doto bb
-             (.order (ByteOrder/LITTLE_ENDIAN))
-             (.put (byte-array (:chunkdata dat)) 0 (:size dat))
-             (.flip)
-             )
-        ;; side effecting
-        gb #(.get bb)
-        gi #(.getInt bb)
-
-        sb #(do (gb) %)
-        si #(do (gi) %)
-        
+  (let [ai (partial assoc-i (:chunkdata  dat))
+        ab (partial assoc-b (:chunkdata  dat))
+        o  #(- % 84)
         ]
     (-> dat
 
-        ;; order matters
-        (assoc :ospt (gb))
-        (assoc :rootnote (gb))
-        (assoc :finetune (gb))
-        (assoc :pan (gb))
-        (assoc :volume-adjust (gb))
-        sb
-        (assoc :start-note (gb))
-        (assoc :end-note (gb))
-        sb
-        (assoc :min-vel (gb))
-        (assoc :max-vel (gb))
-        sb
-        
+        (ab :rootnote 1)
+        (ab :finetune 2)
+        (ab :pan 3)
+        (ab :volume-adjust 4)
+        (ab :start-note 6)
+        (ab :end-note 7)
+        (ab :min-vel 9)
+        (ab :max-vel 10)
+        (ai :sample-start 12)
+        (ai :sample-end 16)
+        (ai :loop-start 20)
+        (ai :loop-end 24)
+        (ab :loop 33)
+        ;; wonder what else is in here?!
+        (ai :group (o 172))
+        (ai :sample-index (o 176))
         (dissoc :chunkdata)
-        )))
+        ))
+  )
+
+(defmethod unpack-chunk :sample [dat]
+  (let [ai (partial assoc-i (:chunkdata  dat))
+        ab (partial assoc-b (:chunkdata  dat))
+        ast (partial assoc-s (:chunkdata  dat))
+        o  #(- % 84)
+        ]
+    (-> dat
+        (ai :length 4)
+        (ai :rate 8)
+        (ai :bitdepth 12)
+        (ast :path 80)
+        (ast :file (+ 80 256))
+        ;; there's the path at 80 and sample name at 336 which is 80 + 256 so...
+        (dissoc :chunkdata)
+        ))
+  )
 
 (defmethod unpack-chunk :default [dat]
   (-> dat
@@ -118,9 +150,24 @@
       )
     ))
 
+
+(comment loop [d (->>  (read-file "/Users/paul/Music/Audio Music Apps/Sampler Instruments/TryLinn2.exs")
+                       (filter #(= (:sig %) :sample))
+                       first
+                       :chunkdata
+                       ;; (filter #(> % 0))
+                       (map #(if (> % 0) (char %) 0))
+
+                       )
+               i 0
+               ]
+         (if (empty? d) 0
+             (do
+               (println i (first d))
+               (recur (rest d) (inc i))
+               ))
+         )
+
 (->  (read-file "/Users/paul/Music/Audio Music Apps/Sampler Instruments/Linn Drum Machine.exs")
-     :chunks
-     (#(map (juxt :sig :unk) %)))
 
-(read-file "/Users/paul/Music/Audio Music Apps/Sampler Instruments/Linn Drum Machine.exs")
-
+     )
